fffc0867cacc120da1f018b65735b323f5b6ecd4
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-01 16:29:35 -0400

    Implemented statement my variable incrementation and decrementation

--- src/com/timbuchalka/Statements.java
+++ src/com/timbuchalka/Statements.java
@@ -4,5 +4,20 @@ public class Statements {
 
     public static void main(String[] args) {
 
+        int myVariable = 50;
+        if (myVariable == 50) {
+            System.out.println("Printed");
+        }
+
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");;
+
+        System.out.println("This is" +
+                "another " +
+                "still more."
+                );
+
+
     }
 }
