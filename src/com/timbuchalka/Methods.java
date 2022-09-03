6b6803cf6ffbb2e004f742c5372ad64522900bec
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-03 19:32:54 -0400

    Implemented a return statement

--- src/com/timbuchalka/Methods.java
+++ src/com/timbuchalka/Methods.java
@@ -17,20 +17,18 @@ public class Methods {
         levelCompleted = 5;
         bonus = 100;
 
-        if (gameOver) {
-            int finalScore = newScore + (newLevelCompleted * newBonus);
-            System.out.println("Your final score was " + finalScore);
-        }
-
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
+            return finalScore;
         }
+
+        return -1;
 
     }
 }
