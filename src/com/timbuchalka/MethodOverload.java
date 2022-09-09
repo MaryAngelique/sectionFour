5a2459656ee23f7f3eeec036015f916ff4307a0f
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-09 11:30:21 -0400

    Implemented calculate score method for the integer score only

--- src/com/timbuchalka/MethodOverload.java
+++ src/com/timbuchalka/MethodOverload.java
@@ -4,10 +4,16 @@ public class MethodOverload {
 
     public static void main(String[] args) {
 
-    }
 
+
+    }
     public static int calculateScore(String playerName, int score){
         System.out.println("Player " + playerName + "score " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score){
+        System.out.println("Unnamed player scored " + score + "points");
         return score * 1000;
     }
 
