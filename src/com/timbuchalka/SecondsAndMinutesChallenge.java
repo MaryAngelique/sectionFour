bbaa6a11d591b0ce16ca73e5b4f38215c9acc8de
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-11 14:07:34 -0400

    Implemented getDurationString

--- src/com/timbuchalka/SecondsAndMinutesChallenge.java
+++ src/com/timbuchalka/SecondsAndMinutesChallenge.java
@@ -19,5 +19,16 @@ public class SecondsAndMinutesChallenge {
     // For the input 61 minutes output should be 01h 01m 00s, but it is ok if it is 1h 1m 0s (Tip: use if-else)
     // Create a new console project and call it SecondsAndMinutesChallenge
 
+    private static String getDurationString(long minutes, long seconds) {
+        if((minutes < 0) || (seconds <0) || (seconds > 59)) {
+            return "Invalid value";
+        }
+
+        long hours = minutes / 60;
+        long remainingMinutes = minutes % 60;
+        return hours + "h " +remainingMinutes + "m " + seconds + "s";
+    }
+
+
 
 }
